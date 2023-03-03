c67410fbfd9972982df040e77684e33d07a71ae7
andy <andy.kdry98@gmail.com>
2023-03-03 21:44:07 +0100

    create table

--- main.py
+++ main.py
@@ -3,3 +3,31 @@ from pathlib import Path
 
 ##create empty database
 Path("bank.db").touch()
+
+##connect db
+conn = sqlite3.connect("bank.db")
+c = conn.cursor()
+
+##create table if not exist
+c.execute(
+    """CREATE TABLE IF NOT EXISTS bank(
+    age int,
+    job varchar(255),
+    marital varchar(255),
+    education varchar(255),
+    default_e text,
+    balance int,
+    housing text,
+    loan text
+    contact text,
+    day int,
+    month text,
+    duration int,
+    campaign int,
+    pdays text,
+    previous int,
+    poutcome text,
+    deposit text
+    );
+    """
+)
