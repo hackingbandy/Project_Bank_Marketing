e8ea65f9807b7c8f55fa3f1edf4839a34b10ffe3
andy <andy.kdry98@gmail.com>
2023-03-03 22:02:32 +0100

    add venv to gitignore

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ Path("bank.db").touch()
 ##connect db
 conn = sqlite3.connect("bank.db")
 c = conn.cursor()
+print(conn)
+print(c)
 
-##create table if not exist
+##create table if not exist (default => default_e)
 c.execute(
     """CREATE TABLE IF NOT EXISTS bank(
     age int,
     job varchar(255),
     marital varchar(255),
     education varchar(255),
-    default_e text,
+    default_e text, 
     balance int,
     housing text,
     loan text
